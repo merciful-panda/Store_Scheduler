0daff3e88380db5919cb9a83b855148c4df852eb
pereabra <pereabra@102500-W>
2018-08-16 19:29:09 -0500

    Add project files.

---
+++ Store_Scheduler/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Store_Scheduler.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
